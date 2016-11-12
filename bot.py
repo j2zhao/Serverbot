b0f9b11ed241b6c8f52d061b67d96ad7da4ab5ff
Charles Lu <charleslu@live.com>
2016-11-12 18:21:39 -0500

    Fix bugs

--- bot.py
+++ bot.py
@@ -2,16 +2,14 @@ import nltk
 
 def respond(message):
     stemmer = nltk.stem.porter.PorterStemmer()
-    tokens = [stemmer.stem(token) for token in nltk.word_tokenize(message)]
+    tokens = [stemmer.stem(token).lower() for token in nltk.word_tokenize(message)]
 
     if tokens_in_list(['load'], tokens):
         if 'balenc' in tokens:
             return 'ans1'
         elif 'firewal':
             return 'ans2'
-
-#=== example response code
-    if tokens_in_list(['price', 'cost', 'pay'], tokens):
+    elif tokens_in_list(['price', 'cost', 'pay'], tokens):
         return 'You are paying ${} per month'.format(40)
     elif 'test' in tokens:
         return 'test'
@@ -19,7 +17,7 @@ def respond(message):
         return "I couldn't understand you, sorry."
 
 def tokens_in_list(search_tokens, tokens):
-    t.lower() for t in search_tokens:
+    for t in search_tokens:
         if t in tokens:
             return True
     else:
