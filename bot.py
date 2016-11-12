a97c01dcae44e2b76bf428a9c2b63c8502b87469
Adeniji Ogunlana <adeniji.ogunlana@gmail.com>
2016-11-12 18:05:53 -0500

    added test case

--- bot.py
+++ bot.py
@@ -3,7 +3,14 @@ import nltk
 def respond(message):
     stemmer = nltk.stem.porter.PorterStemmer()
     tokens = [stemmer.stem(token) for token in nltk.word_tokenize(message)]
-    
+
+    if tokens_in_list(['load'], tokens):
+        if 'balenc' in tokens:
+            return 'ans1'
+        elif 'firewal':
+            return 'ans2'
+
+#=== example response code
     if tokens_in_list(['price', 'cost', 'pay'], tokens):
         return 'You are paying ${} per month'.format(40)
     elif 'test' in tokens:
@@ -12,7 +19,7 @@ def respond(message):
         return "I couldn't understand you, sorry."
 
 def tokens_in_list(search_tokens, tokens):
-    for t in search_tokens:
+    t.lower() for t in search_tokens:
         if t in tokens:
             return True
     else:
