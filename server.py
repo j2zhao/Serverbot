71d97d3562cdb6c4274672abfa0bf6fad9aa87ae
Charles Lu <charleslu@live.com>
2016-11-12 18:07:22 -0500

    Fix bytes/str bug

--- server.py
+++ server.py
@@ -42,7 +42,7 @@ def messaging_events(payload):
     messaging_events = data['entry'][0]['messaging']
     for event in messaging_events:
         if 'message' in event and 'text' in event['message']:
-            yield event['sender']['id'], event['message']['text'].encode('unicode_escape')
+            yield event['sender']['id'], event['message']['text'].encode()
         else:
             yield event['sender']['id'], 'I can\'t echo this'
 
