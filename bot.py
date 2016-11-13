57e3e48936d08a4a8a9a06d31fca1c5091b2eaa7
Adeniji Ogunlana <adeniji.ogunlana@gmail.com>
2016-11-12 19:57:38 -0500

    added first server name function

--- bot.py
+++ bot.py
@@ -1,29 +1,22 @@
 import nltk
+import 1a1client.py
 
 def respond(message):
     stemmer = nltk.stem.porter.PorterStemmer()
     tokens = [stemmer.stem(token).lower() for token in nltk.word_tokenize(message)]
 
     if tokens_in_list(['hi', 'hello', 'hey', 'sup', 'whatsup', 'howdy', 'yo', 'hello Chatterbox', 'help', 'can you help me?'], tokens);
-    return 'Hello! how can I be of assistance to you?'
+    return 'Hello! How can I be of assistance to you?'
 
-    if tokens_in_list(['load'], tokens):
-        if 'balanc' in tokens:
-            return 'ans1'
-        elif 'firewal' in tokens:
+    if tokens_in_list(['server'], tokens):
+        if 'name' in tokens:
+            return 'The server name is ' + getServerNames()
+        elif 'id' in tokens:
             return 'ans2'
-        elif 'hardware' in tokens:
+        elif 'status' in tokens:
             return 'ans3'
-        elif 'SSD' in tokens:
+        elif 'HDD' in tokens:
             return 'ans4'
-        elif 'image' in tokens:
-            return 'ans5'
-        elif 'ip' in tokens:
-            return 'ans6'
-        elif ['private', 'network'] in tokens:
-            return 'ans7'
-        elif 'all' in tokens:
-            return 'ans8'
         else:
             return 'all info'
     elif tokens_in_list(['price', 'cost', 'pay'], tokens):
