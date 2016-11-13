e3987627e05e49dc67856e95b89160351576462c
Adeniji Ogunlana <adeniji.ogunlana@gmail.com>
2016-11-12 20:49:58 -0500

    second function fix

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ def respond(message):
     elif tokens_in_list(['server'], tokens):
         if 'name' in tokens:
             return 'The server names are: ' + ', '.join(oneclient.getServerNames())
-        elif 'id' in tokens:
-            return 'The server IDs are: ' + ', '.join(oneclient.getServerIds())
+        elif 'id' in tokens or 'ids' in tokens:
+            return 'The server IDs are: ' + ', '.join(oneclient.getServerIDs())
         elif 'status' in tokens:
             return 'The server status is: '
         elif 'HDD' in tokens:
